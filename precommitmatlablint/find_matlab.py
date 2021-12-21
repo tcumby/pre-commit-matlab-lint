9394ad562b2a669db425c8476489ee15744fa8ac
Ty <ty.cumby@gmail.com>
2021-12-21 11:26:26 -0700

    Minor refactor and add docstrigns

--- precommitmatlablint/find_matlab.py
+++ precommitmatlablint/find_matlab.py
@@ -7,31 +7,68 @@ from typing import List, Optional, Tuple
 from precommitmatlablint.return_code import ReturnCode
 
 
-def get_matlab_installs() -> List[Path]:
-    this_platform = sys.platform
-    install_root_paths: List[Path]
-    if "win32" == this_platform:
-        program_files: str = os.getenv("PROGRAMFILES", "C:\\Program Files")
-        root_path = Path(program_files, "MATLAB")
-        install_root_paths = sorted(root_path.glob(r"R\d+\w"))
-    elif "darwin" == this_platform:
-        root_path = Path("/Applications")
-        install_root_paths = sorted(root_path.glob(r"MATLAB_R\d+\w"))
-    elif "linux" == this_platform:
-        root_path = Path("/usr", "local", "MATLAB")
-        install_root_paths = sorted(root_path.glob(r"R\d+\w"))
-    else:
-        root_path = Path("/usr", "local", "MATLAB")
-        install_root_paths = sorted(root_path.glob(r"R\d+\w"))
+def get_matlab_root(platform: str) -> Path:
+    """Return the MATLAB install root folder path, e.g. C:\\Program Files\\MATLAB on Windows
 
-    return install_root_paths
+    Parameters
+    ----------
+    platform: str
+              The value of sys.platform
+
+    Returns
+    -------
+    Path
+    """
+
+    root_paths = {
+        "win32": Path(os.getenv("PROGRAMFILES", "C:\\Program Files"), "MATLAB"),
+        "darwin": Path("/Applications"),
+        "linux": Path("/usr", "local", "MATLAB"),
+    }
+
+    return root_paths[platform]
+
+
+def get_matlab_installs() -> List[Path]:
+    """Return a list of all MATLAB home folder paths based on matching the release name pattern in the folder name.
+
+    Returns
+    -------
+    List of Path
+                 A list of all discovered MATLAB home paths
+    """
+    this_platform = sys.platform
+    matlab_home_paths: List[Path] = []
+    root_path = get_matlab_root(this_platform)
+    if "win32" == this_platform:
+        matlab_home_paths = sorted(root_path.glob(r"R\d+\w"))
+    elif "darwin" == this_platform:
+        matlab_home_paths = sorted(root_path.glob(r"MATLAB_R\d+\w"))
+    elif "linux" == this_platform:
+        matlab_home_paths = sorted(root_path.glob(r"R\d+\w"))
+
+    return matlab_home_paths
 
 
 def validate_matlab_path(path: Path) -> bool:
+    """Validate whether a potential MATLAB executable path is legitimate or not."""
     return path.exists() and path.is_file()
 
 
 def find_matlab_release(release: str, search_path: List[Path]) -> Optional[Path]:
+    """Find the path to a MATLAB executable specified by the release name (e.g. R2021a).
+
+    Parameters
+    ----------
+    release: str
+                The desired MATLAB release name
+    search_path: list of Path
+                 The list of MATLAB home paths to search through
+
+    Returns
+    -------
+    Path, optional
+                   The full path to the MATLAB executable, if found"""
     matlab_path: Optional[Path] = None
 
     # The MATLAB folder path contains the release name in the root folder,
@@ -47,18 +84,21 @@ def find_matlab_release(release: str, search_path: List[Path]) -> Optional[Path]
 
 
 def get_arch_folder_name() -> str:
+    """Return the MATLAB architecture folder name."""
     arch_folders = {"win32": "win64", "darwin": "maci64", "linux": "glnxa64"}
 
     return arch_folders[sys.platform]
 
 
 def get_matlab_exe_name() -> str:
+    """Return the MATLAB executable file name."""
     matlab_exe: str = "matlab.exe" if sys.platform == "win32" else "matlab"
     return matlab_exe
 
 
 def query_matlab_version(matlab_exe_path: Path) -> str:
-    matlab_command: str = "disp(version);quit"
+    """Query a given MATLAB instance for its version."""
+    matlab_command: str = "clc;disp(version);quit"
     version_string: str = ""
     command: List[str] = [str(matlab_exe_path), "-nosplash", "-nodesktop"]
     if sys.platform == "win32":
@@ -79,6 +119,19 @@ def query_matlab_version(matlab_exe_path: Path) -> str:
 
 
 def find_matlab_version(target_version: str, search_path: List[Path]) -> Optional[Path]:
+    """Find the path to a MATLAB executable specified by its version number.
+
+    Parameters
+    __________
+    target_version: str
+                        The desired MATLAB version (e.g. 9.10)
+    search_path: list of Path
+                        The list of all MATLAB home folder paths
+
+    Returns
+    _______
+    Path, optional
+                   The absolute file path to the MATLAB executable, if found"""
     matlab_path: Optional[Path] = None
     matlab_exe_name = get_matlab_exe_name()
 
@@ -98,6 +151,25 @@ def find_matlab(
     matlab_version: Optional[str],
     matlab_release_name: Optional[str],
 ) -> Tuple[Optional[Path], ReturnCode]:
+    """Find the path to a MATLAB executable by providing a path for validation, release name, or version.
+
+    Note that it is only necessary to supply either one of the following: a path, a version or a release name.
+
+    Parameters
+    ----------
+    potential_matlab_path: Path, optional
+                                            An absolute path to a MATLAB executable to validate.
+    matlab_version: str, optional
+                                            The desired MATLAB version.
+    matlab_release_name: str, optional
+
+    Returns
+    -------
+    matlab_path: Path, optional
+                    The absolute path to the MATLAB executable
+    return_code: ReturnCode
+    """
+
     return_code: ReturnCode = ReturnCode.FAIL
     matlab_path: Optional[Path] = None
 
