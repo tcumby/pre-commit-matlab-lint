27e36e9395d696679f20877e830f3c907f566272
Ty <ty.cumby@gmail.com>
2022-01-01 21:03:24 -0700

    Ensuring that the cache file is not present when a given test runs.

--- tests/test_find_matlab.py
+++ tests/test_find_matlab.py
@@ -73,26 +73,28 @@ class TestFindMatlab:
             assert len(handle_list2) > 0
 
     def test_handle_list_prune(self):
-        handle_list = MatlabHandleList()
-        install_list = get_matlab_installs()
-        assert len(install_list) > 0
-        handle_list.update(install_list)
+        with TemporaryDirectory() as temp_dir:
+            cache_file = Path(temp_dir) / "cache_file.yaml"
+            handle_list = MatlabHandleList(cache_file)
+            install_list = get_matlab_installs()
+            assert len(install_list) > 0
+            handle_list.update(install_list)
 
-        expected_length = len(handle_list)
+            expected_length = len(handle_list)
 
-        # create a bogus MatlabHandle
-        fake_home = Path().absolute()
-        fake_exe = MatlabHandle.construct_exe_path(fake_home)
-        fake_handle = MatlabHandle(home_path=fake_home, exe_path=fake_exe)
+            # create a bogus MatlabHandle
+            fake_home = Path().absolute()
+            fake_exe = MatlabHandle.construct_exe_path(fake_home)
+            fake_handle = MatlabHandle(home_path=fake_home, exe_path=fake_exe)
 
-        handle_list.append(fake_handle)
+            handle_list.append(fake_handle)
 
-        assert len(handle_list) > expected_length
+            assert len(handle_list) > expected_length
 
-        handle_list.prune()
+            handle_list.prune()
 
-        assert expected_length == len(handle_list)
-        assert handle_list.has_changes
+            assert expected_length == len(handle_list)
+            assert handle_list.has_changes
 
     def test_query_matlab_version(self):
         install_list = get_matlab_installs()
@@ -108,85 +110,95 @@ class TestFindMatlab:
         assert match is not None
 
     def test_find_matlab_version(self):
-        handle_list = MatlabHandleList()
-        install_list = get_matlab_installs()
-        assert len(install_list) > 0
-        handle_list.update(install_list)
+        with TemporaryDirectory() as temp_dir:
+            cache_file = Path(temp_dir) / "cache_file.yaml"
+            handle_list = MatlabHandleList(cache_file)
+            install_list = get_matlab_installs()
+            assert len(install_list) > 0
+            handle_list.update(install_list)
 
-        this_matlab_home = install_list[0]
-        version_info_file = this_matlab_home / "VersionInfo.xml"
-        expected_version, expected_release = MatlabHandle.read_version_info(version_info_file)
+            this_matlab_home = install_list[0]
+            version_info_file = this_matlab_home / "VersionInfo.xml"
+            expected_version, expected_release = MatlabHandle.read_version_info(version_info_file)
 
-        matlab_handle = handle_list.find_version(expected_version)
-        assert matlab_handle is not None
+            matlab_handle = handle_list.find_version(expected_version)
+            assert matlab_handle is not None
 
-        assert expected_version == matlab_handle.version
-        assert expected_release == matlab_handle.release
+            assert expected_version == matlab_handle.version
+            assert expected_release == matlab_handle.release
 
     def test_find_matlab_release(self):
-        handle_list = MatlabHandleList()
-        install_list = get_matlab_installs()
-        assert len(install_list) > 0
-        handle_list.update(install_list)
+        with TemporaryDirectory() as temp_dir:
+            cache_file = Path(temp_dir) / "cache_file.yaml"
+            handle_list = MatlabHandleList(cache_file)
+            install_list = get_matlab_installs()
+            assert len(install_list) > 0
+            handle_list.update(install_list)
 
-        this_matlab_home = install_list[0]
-        version_info_file = this_matlab_home / "VersionInfo.xml"
-        expected_version, expected_release = MatlabHandle.read_version_info(version_info_file)
+            this_matlab_home = install_list[0]
+            version_info_file = this_matlab_home / "VersionInfo.xml"
+            expected_version, expected_release = MatlabHandle.read_version_info(version_info_file)
 
-        matlab_handle = handle_list.find_release(expected_release)
-        assert matlab_handle is not None
+            matlab_handle = handle_list.find_release(expected_release)
+            assert matlab_handle is not None
 
-        assert expected_version == matlab_handle.version
-        assert expected_release == matlab_handle.release
+            assert expected_version == matlab_handle.version
+            assert expected_release == matlab_handle.release
 
     def test_find_matlab_with_release(self):
-        handle_list = MatlabHandleList()
-        install_list = get_matlab_installs()
-        assert len(install_list) > 0
-        handle_list.update(install_list)
+        with TemporaryDirectory() as temp_dir:
+            cache_file = Path(temp_dir) / "cache_file.yaml"
+            handle_list = MatlabHandleList(cache_file)
+            install_list = get_matlab_installs()
+            assert len(install_list) > 0
+            handle_list.update(install_list)
 
-        this_matlab_home = install_list[0]
-        version_info_file = this_matlab_home / "VersionInfo.xml"
-        expected_version, expected_release = MatlabHandle.read_version_info(version_info_file)
+            this_matlab_home = install_list[0]
+            version_info_file = this_matlab_home / "VersionInfo.xml"
+            expected_version, expected_release = MatlabHandle.read_version_info(version_info_file)
 
-        found_matlab, return_code = find_matlab(matlab_release_name=expected_release)
-        assert found_matlab is not None
-        assert return_code == ReturnCode.OK
+            found_matlab, return_code = find_matlab(matlab_release_name=expected_release)
+            assert found_matlab is not None
+            assert return_code == ReturnCode.OK
 
-        assert expected_release == found_matlab.release
-        assert expected_version == found_matlab.version
+            assert expected_release == found_matlab.release
+            assert expected_version == found_matlab.version
 
     def test_find_matlab_with_version(self):
-        handle_list = MatlabHandleList()
-        install_list = get_matlab_installs()
-        assert len(install_list) > 0
-        handle_list.update(install_list)
+        with TemporaryDirectory() as temp_dir:
+            cache_file = Path(temp_dir) / "cache_file.yaml"
+            handle_list = MatlabHandleList(cache_file)
+            install_list = get_matlab_installs()
+            assert len(install_list) > 0
+            handle_list.update(install_list)
 
-        this_matlab_home = install_list[0]
-        version_info_file = this_matlab_home / "VersionInfo.xml"
-        expected_version, expected_release = MatlabHandle.read_version_info(version_info_file)
+            this_matlab_home = install_list[0]
+            version_info_file = this_matlab_home / "VersionInfo.xml"
+            expected_version, expected_release = MatlabHandle.read_version_info(version_info_file)
 
-        found_matlab, return_code = find_matlab(matlab_version=expected_version)
-        assert found_matlab is not None
-        assert return_code == ReturnCode.OK
+            found_matlab, return_code = find_matlab(matlab_version=expected_version)
+            assert found_matlab is not None
+            assert return_code == ReturnCode.OK
 
-        assert expected_version == found_matlab.version
+            assert expected_version == found_matlab.version
 
     def test_find_matlab_with_path(self):
-        handle_list = MatlabHandleList()
-        install_list = get_matlab_installs()
-        assert len(install_list) > 0
-        handle_list.update(install_list)
+        with TemporaryDirectory() as temp_dir:
+            cache_file = Path(temp_dir) / "cache_file.yaml"
+            handle_list = MatlabHandleList(cache_file)
+            install_list = get_matlab_installs()
+            assert len(install_list) > 0
+            handle_list.update(install_list)
 
-        this_matlab_home = install_list[0]
-        version_info_file = this_matlab_home / "VersionInfo.xml"
-        expected_version, expected_release = MatlabHandle.read_version_info(version_info_file)
+            this_matlab_home = install_list[0]
+            version_info_file = this_matlab_home / "VersionInfo.xml"
+            expected_version, expected_release = MatlabHandle.read_version_info(version_info_file)
 
-        found_matlab, return_code = find_matlab(matlab_home_path=this_matlab_home)
-        assert found_matlab is not None
-        assert return_code == ReturnCode.OK
+            found_matlab, return_code = find_matlab(matlab_home_path=this_matlab_home)
+            assert found_matlab is not None
+            assert return_code == ReturnCode.OK
 
-        assert expected_version == found_matlab.version
+            assert expected_version == found_matlab.version
 
     def test_find_matlab_with_fake_path(self):
         fake_matlab = Path().absolute()
@@ -195,28 +207,34 @@ class TestFindMatlab:
         assert return_code == ReturnCode.FAIL
 
     def test_fake_version_find_matlab_version(self):
-        handle_list = MatlabHandleList()
-        install_list = get_matlab_installs()
-        assert len(install_list) > 0
-        handle_list.update(install_list)
+        with TemporaryDirectory() as temp_dir:
+            cache_file = Path(temp_dir) / "cache_file.yaml"
+            handle_list = MatlabHandleList(cache_file)
+            install_list = get_matlab_installs()
+            assert len(install_list) > 0
+            handle_list.update(install_list)
 
-        fake_version = "0.00"
-        found_matlab = handle_list.find_version(fake_version)
+            fake_version = "0.00"
+            found_matlab = handle_list.find_version(fake_version)
 
-        assert found_matlab is None
+            assert found_matlab is None
 
     def test_fake_release_find_matlab_release(self):
-        handle_list = MatlabHandleList()
-        install_list = get_matlab_installs()
-        assert len(install_list) > 0
-        handle_list.update(install_list)
+        with TemporaryDirectory() as temp_dir:
+            cache_file = Path(temp_dir) / "cache_file.yaml"
+            handle_list = MatlabHandleList(cache_file)
+            install_list = get_matlab_installs()
+            assert len(install_list) > 0
+            handle_list.update(install_list)
 
-        fake_release = "R0000a"
-        found_matlab = handle_list.find_release(fake_release)
+            fake_release = "R0000a"
+            found_matlab = handle_list.find_release(fake_release)
 
-        assert found_matlab is None
+            assert found_matlab is None
 
     def test_fake_version_find_matlab(self):
+        handle_list = MatlabHandleList()
+        handle_list.cache_file.unlink(missing_ok=True)
         fake_version = "0.00"
         found_matlab, return_code = find_matlab(matlab_version=fake_version)
 
@@ -224,6 +242,8 @@ class TestFindMatlab:
         assert return_code == ReturnCode.FAIL
 
     def test_fake_release_find_matlab(self):
+        handle_list = MatlabHandleList()
+        handle_list.cache_file.unlink(missing_ok=True)
         fake_release = "R0000a"
         found_matlab, return_code = find_matlab(matlab_release_name=fake_release)
 
