ccd1613a939821e5dc4235ce888f8d84b411bb89
Ty <ty.cumby@gmail.com>
2022-01-09 21:32:15 -0700

    A few tests of the CLI

--- tests/test_lint_matlab.py
+++ tests/test_lint_matlab.py
@@ -5,7 +5,7 @@ import pytest  # noqa: F401 # pylint: disable=unused-import
 from pathlib import Path
 
 from precommitmatlablint.find_matlab import MatlabHandleList, get_matlab_installs, MatlabHandle
-from precommitmatlablint.lint_matlab import validate_matlab
+from precommitmatlablint.lint_matlab import validate_matlab, main
 from precommitmatlablint.return_code import ReturnCode
 
 
@@ -21,6 +21,30 @@ def handle_list(request):
         cache_file = Path(temp_dir) / "cache_file.yaml"
         handle_list = MatlabHandleList(cache_file)
         yield handle_list
+
+
+def prepare_command(
+    enable_cyc: bool,
+    enable_mod_cyc: bool,
+    fail_warnings: bool,
+    filepaths: List[Path],
+    version: str,
+    ignore_ok_pragmas: bool,
+):
+    command: List[str] = [f"--matlab-version={version}"]
+    if enable_cyc:
+        command.append("--enable-cyclomaticity")
+    if enable_mod_cyc:
+        command.append("--enable-modified-cyclomaticit")
+    if ignore_ok_pragmas:
+        command.append("--ignore-ok-pragmas")
+    if fail_warnings:
+        command.append("--treat-warning-as-error")
+
+    command_paths: List[str] = [str(p) for p in filepaths]
+
+    command += command_paths
+    return command
 
 
 class TestLintMatlab:
@@ -222,6 +246,48 @@ class TestLintMatlab:
     @pytest.mark.parametrize(
         "filename_list",
         [
+            ["invalid_char.m", "clean_function.m", "clean_function.m"],
+            ["clean_function.m", "invalid_char.m", "clean_function.m"],
+            ["clean_function.m", "clean_function.m", "invalid_char.m"],
+        ],
+    )
+    def one_bad_file_through_main(
+        self,
+        matlab_folder_path: Path,
+        handle_list,
+        enable_cyc: bool,
+        enable_mod_cyc: bool,
+        ignore_ok_pragmas: bool,
+        fail_warnings: bool,
+        filename_list: List[str],
+    ):
+        assert matlab_folder_path.exists()
+        filepaths: List[Path] = [matlab_folder_path / s for s in filename_list]
+        assert all([s.exists() for s in filepaths])
+
+        install_list = get_matlab_installs()
+        assert len(install_list) > 0
+
+        this_matlab_home = install_list[0]
+        matlab_exe: Path = MatlabHandle.construct_exe_path(this_matlab_home)
+        handle = MatlabHandle(home_path=this_matlab_home, exe_path=matlab_exe)
+        assert handle.is_initialized()
+
+        command = prepare_command(
+            enable_cyc, enable_mod_cyc, fail_warnings, filepaths, handle.version, ignore_ok_pragmas
+        )
+
+        return_code: int = main(command)
+
+        assert int(ReturnCode.FAIL) == return_code
+
+    @pytest.mark.parametrize("enable_cyc", [True, False])
+    @pytest.mark.parametrize("enable_mod_cyc", [True, False])
+    @pytest.mark.parametrize("ignore_ok_pragmas", [True, False])
+    @pytest.mark.parametrize("fail_warnings", [True, False])
+    @pytest.mark.parametrize(
+        "filename_list",
+        [
             ["invalid_char.m", "invalid_char.m", "clean_function.m"],
             ["clean_function.m", "invalid_char.m", "invalid_char.m"],
             ["invalid_char.m", "clean_function.m", "invalid_char.m"],
@@ -260,6 +326,48 @@ class TestLintMatlab:
         )
 
         assert ReturnCode.FAIL == return_code
+
+    @pytest.mark.parametrize("enable_cyc", [True, False])
+    @pytest.mark.parametrize("enable_mod_cyc", [True, False])
+    @pytest.mark.parametrize("ignore_ok_pragmas", [True, False])
+    @pytest.mark.parametrize("fail_warnings", [True, False])
+    @pytest.mark.parametrize(
+        "filename_list",
+        [
+            ["invalid_char.m", "invalid_char.m", "clean_function.m"],
+            ["clean_function.m", "invalid_char.m", "invalid_char.m"],
+            ["invalid_char.m", "clean_function.m", "invalid_char.m"],
+        ],
+    )
+    def one_good_file_through_main(
+        self,
+        matlab_folder_path: Path,
+        handle_list,
+        enable_cyc: bool,
+        enable_mod_cyc: bool,
+        ignore_ok_pragmas: bool,
+        fail_warnings: bool,
+        filename_list: List[str],
+    ):
+        assert matlab_folder_path.exists()
+        filepaths: List[Path] = [matlab_folder_path / s for s in filename_list]
+        assert all([s.exists() for s in filepaths])
+
+        install_list = get_matlab_installs()
+        assert len(install_list) > 0
+
+        this_matlab_home = install_list[0]
+        matlab_exe: Path = MatlabHandle.construct_exe_path(this_matlab_home)
+        handle = MatlabHandle(home_path=this_matlab_home, exe_path=matlab_exe)
+        assert handle.is_initialized()
+
+        command = prepare_command(
+            enable_cyc, enable_mod_cyc, fail_warnings, filepaths, handle.version, ignore_ok_pragmas
+        )
+
+        return_code: int = main(command)
+
+        assert int(ReturnCode.FAIL) == return_code
 
     @pytest.mark.parametrize("ignore_ok_pragmas", [True, False])
     @pytest.mark.parametrize(
